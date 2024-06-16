b1d0bf862201e053b6eea82adabfee145fb52a97
thisiswooyeol <wooyeol0519@snu.ac.kr>
2024-06-17 03:07:38 +0900

    fix: DISCARD convTranspose2d layers for StateDecoder     Assume that the usage of last linear layer in ImageEncoder make convTranspose2d inappropriate     Therefore, used MLP instead

--- reward_induced/src/state_decoder.py
+++ reward_induced/src/state_decoder.py
@@ -9,24 +9,6 @@ class StateDecoder(nn.Module):
     """
     def __init__(self, in_size, out_size):
         super(StateDecoder, self).__init__()
-        # assume square image and H, W should be power of 2
-        # assert image_shape[-1] == image_shape[-2]
-        # self.level = int(log2(image_shape[-1]))
-        # _channels = 2 ** self.level
-        
-        # _layers = []
-        # for _ in range(self.level-1):
-        #     _layers.append(
-        #         nn.ConvTranspose2d(_channels, _channels // 2, kernel_size=3, 
-        #                            stride=2, padding=1, output_padding=1))
-        #     _layers.append(nn.LeakyReLU(0.2))
-        #     _channels //= 2
-        # _layers.append(nn.ConvTranspose2d(_channels, _channels, kernel_size=3, 
-        #                             stride=2, padding=1, output_padding=1))
-        # _layers.append(nn.LeakyReLU(0.2))
-        # _layers.append(nn.ConvTranspose2d(_channels, 3, kernel_size=3, 
-        #                             stride=1, padding=1))        
-        # self.convs = nn.Sequential(*_layers)
         HIDDEN_SIZE = 128
         self.out_size = out_size
 
@@ -35,19 +17,13 @@ class StateDecoder(nn.Module):
             nn.ReLU(),
             nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
             nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+            nn.ReLU(),
             nn.Linear(HIDDEN_SIZE, out_size*out_size),
+            nn.Tanh(),
         )
         
     def forward(self, x):
-        # # x: (B, L, C) 
-        # B = x.shape[0]
-        # L = x.shape[1]
-        # x = x.reshape(B*L, x.shape[2], 1, 1) # (B, L, C) -> (B*L, C, 1, 1)
-        
-        # y = self.convs(x)
-        # y = y.reshape(B, L, 3, y.shape[-1], y.shape[-1])
-        # return y
-
         B = x.shape[0]
         L = x.shape[1]
         x = self.layers(x)
