d4d039ec7e075e32050fbaf1f0644f567263a0cd
thisiswooyeol <wooyeol0519@snu.ac.kr>
2024-06-23 02:50:43 +0900

    feat: VecEnv setup feat: ActorCritic agent with  method (for old policy) feat: enable rollout with RolloutBuffer

--- ppo/model.py
+++ ppo/model.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+
 class CNN(nn.Module):
     """
     3-layer CNN to encode image
@@ -39,3 +43,39 @@ class MLP(nn.Module):
 
     def forward(self, x):
         return self.mlp(x)
+
+
+class MLPActorCritic(nn.Module):
+    """
+    Actor-Critic agent with 2-layer MLPs
+    """
+    def __init__(self, repr_dim, hidden_size, action_dim, action_std=0.5, encoder=None):
+        super(MLPActorCritic, self).__init__()
+        self.actor = nn.Sequential(
+            nn.Linear(repr_dim, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, action_dim),
+            nn.Tanh()
+        )
+        self.critic = nn.Sequential(
+            nn.Linear(repr_dim, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+        self.encoder = encoder
+
+        self.action_dim = action_dim
+        self.action_var = torch.full((action_dim,), action_std**2).to(device)
+
+    def get_action(self, obs):
+        if self.encoder is not None:
+            obs = self.encoder(obs)
+        action_mean = self.actor(obs)
+        cov_mat = torch.diag(self.action_var).unsqueeze(0)
+        dist = torch.distributions.MultivariateNormal(action_mean, cov_mat)
+
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+        state_value = self.critic(obs)
+
+        return action.detach(), action_logprob.detach(), state_value.detach()
--- ppo/ppo.py
+++ ppo/ppo.py
@@ -1,13 +1,43 @@
-import gym.vector
+import gymnasium as gym
 import torch
 import torch.nn as nn
+import numpy as np
 
-import gym
 import sprites_env
-from typing import Any, Dict, Union
+from typing import Union
 
-from .model import CNN, MLP
+from .model import CNN, MLP, MLPActorCritic
 from reward_induced.src.reward_predictor import RewardPredictor, ImageEncoder
+
+
+class RolloutBuffer:
+    def __init__(self, n_steps, n_envs, obs_shape, action_shape, device):
+        self.buffer_len = n_steps // n_envs
+        self.current_step = 0
+        self.obs = torch.zeros((self.buffer_len + 1, n_envs, *obs_shape), dtype=torch.float32).to(device)
+        self.actions = torch.zeros((self.buffer_len, n_envs, *action_shape), dtype=torch.float32).to(device)
+        self.rewards = np.zeros((self.buffer_len, n_envs), dtype=np.float32)
+        self.terminals = np.zeros((self.buffer_len, n_envs), dtype=np.float32)
+
+    def append(self, obs, action, reward, terminal):
+        if self.current_step == self.buffer_len:
+            self._append_last_obs(obs)
+        else:
+            self.obs[self.current_step] = obs
+            self.actions[self.current_step] = action
+            self.rewards[self.current_step] = reward
+            self.terminals[self.current_step] = terminal
+            self.current_step += 1
+
+    def _append_last_obs(self, obs):
+        self.obs[-1] = obs
+
+    def reset(self):
+        self.current_step = 0
+        self.obs = torch.zeros_like(self.obs)
+        self.actions = torch.zeros_like(self.actions)
+        self.rewards = np.zeros_like(self.rewards)
+        self.terminals = np.zeros_like(self.terminals)
 
 
 class PPO(nn.Module):
@@ -45,9 +75,9 @@ class PPO(nn.Module):
     """
     def __init__(
             self,
-            policy: str,
+            policy_type: str,
             env: str,
-            num_envs: int = 1,
+            num_envs: int = 2,
             learning_rate: float = 3e-4,
             n_steps: int = 2048,
             batch_size: int = 64,
@@ -60,18 +90,15 @@ class PPO(nn.Module):
     ):
         super(PPO, self).__init__()
         # make gym environment
-        self._policy_env_sanity_check(policy, env)
-        # self.env = gym.vector.make(env, num_envs=num_envs)
-        self.env = gym.make(env)
+        self._policy_env_sanity_check(policy_type, env)
+        self.envs = gym.make_vec(env, num_envs=num_envs)
 
         # By default, use image shape (3, 64, 64), and 64 hidden states
         self.image_shape = (3, 64, 64)
         self.hidden_size = 32
 
-        self.policy = policy
-        self.encoder = self._setup_encoder(policy)
-        self.actor, self.critic = self._setup_actor_critic(policy)
-
+        _encoder = self._setup_encoder(policy_type)
+        self.policy = self._setup_actor_critic(policy_type, encoder=_encoder)
 
         self.learning_rate = learning_rate
         self.n_steps = n_steps
@@ -80,7 +107,8 @@ class PPO(nn.Module):
         self.clip_range = clip_range
         self.tensorboard_log = tensorboard_log
         self.verbose = verbose
-        self.device = device
+        self.seed = seed
+        self.device = device if device != 'auto' else torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     @staticmethod
     def _policy_env_sanity_check(policy, env):
@@ -90,49 +118,43 @@ class PPO(nn.Module):
         elif 'State' in env:
             raise ValueError(f'Invalid policy-environment combination: {policy}-{env}')
 
-    def _setup_encoder(self, policy):
-        if policy == 'cnn':
+    def _setup_encoder(self, policy_type):
+        if policy_type == 'cnn':
             return CNN()
-        elif policy == 'image-scratch':
+        elif policy_type == 'image-scratch':
             return ImageEncoder(self.image_shape, self.hidden_size)
-        elif policy == 'image-reconstruction' \
-            or policy == 'image-reconstruction-finetune':
+        elif policy_type == 'image-reconstruction' \
+            or policy_type == 'image-reconstruction-finetune':
             return ImageEncoder(self.image_shape, self.hidden_size)
             # TODO: load encoder from image reconstruction task
-        elif policy == 'reward-prediction' \
-            or policy == 'reward-prediction-finetune':
+        elif policy_type == 'reward-prediction' \
+            or policy_type == 'reward-prediction-finetune':
             _reward_predictor = RewardPredictor(self.image_shape, 1, 29)
             # TODO: use general method to load encoder from reward prediction task
             _reward_predictor.load_state_dict(torch.load('reward_induced/models/encoder/encoder_dist0_final.pt'))
             _image_encoder = _reward_predictor.image_encoder
-            if policy == 'reward-prediction':
+            if policy_type == 'reward-prediction':
                 _image_encoder.requires_grad_(False)
             return _image_encoder
-        elif policy == 'oracle':
+        elif policy_type == 'oracle':
             return None
         else:
-            raise ValueError(f'Invalid policy: {policy}')
+            raise ValueError(f'Invalid policy: {policy_type}')
         
-    def _setup_actor_critic(self, policy):
-        if policy == 'cnn':
-            in_features = (self.image_shape[-1] // 8 - 1) ** 2 * self.encoder.CHANNELS
-            actor = MLP(in_features, self.hidden_size * 2, self.env.action_space.shape[0])
-            critic = MLP(in_features, self.hidden_size * 2, 1)
-            return actor, critic
-        elif policy == 'image-scratch' \
-            or policy == 'image-reconstruction' \
-            or policy == 'image-reconstruction-finetune' \
-            or policy == 'reward-prediction' \
-            or policy == 'reward-prediction-finetune':
-            in_features = self.hidden_size
-            actor = MLP(in_features, self.hidden_size, self.env.action_space.shape[0])
-            critic = MLP(in_features, self.hidden_size, 1)
-            return actor, critic
-        elif policy == 'oracle':
-            in_features = self.env.observation_space.shape[0]
-            actor = MLP(in_features, self.hidden_size, self.env.action_space.shape[0])
-            critic = MLP(in_features, self.hidden_size, 1)
-            return actor, critic
+    def _setup_actor_critic(self, policy_type, encoder):
+        action_dim = self.envs.single_action_space.shape[0]
+        if policy_type == 'cnn':
+            repr_dim = (self.image_shape[-1] // 8 - 1) ** 2 * self.encoder.CHANNELS
+            return MLPActorCritic(repr_dim, self.hidden_size * 2, action_dim, encoder=encoder)
+        elif policy_type == 'image-scratch' \
+            or policy_type == 'image-reconstruction' \
+            or policy_type == 'image-reconstruction-finetune' \
+            or policy_type == 'reward-prediction' \
+            or policy_type == 'reward-prediction-finetune':
+            return MLPActorCritic(64, self.hidden_size, action_dim, encoder=encoder)
+        elif policy_type == 'oracle':
+            repr_dim = self.envs.single_observation_space.shape[0]
+            return MLPActorCritic(repr_dim, self.hidden_size, action_dim, encoder=encoder)
         
 
     def learn(
@@ -143,19 +165,32 @@ class PPO(nn.Module):
             reset_num_timesteps: bool = True,
             progress_bar: bool = False
     ):
-        obs = self.env.reset()
+        self.policy.to(self.device)
+        buffer = RolloutBuffer(self.n_steps, self.envs.num_envs, (4,), self.envs.single_action_space.shape, self.device)
 
-        for t in range(total_timesteps):
-            action = self.env.action_space.sample() # debug code
-            obs, reward, done, info = self.env.step(action)
+        obs, _ = self.envs.reset(seed=self.seed)
 
-            if t % log_interval == 0:
-                print(f't: {t}, obs: {obs}, reward: {reward}, done: {done}, info: {info}')
+        for update in range(total_timesteps // self.n_steps):
+            # Rollout phase
+            for _ in range(self.n_steps // self.envs.num_envs):
+                obs = torch.FloatTensor(obs).to(self.device)
+                action, action_logprob, state_value = self.policy.get_action(obs)
 
-        self.env.close()
+                new_obs, reward, terminated, _, _ = self.envs.step(action.cpu().numpy())
+                buffer.append(obs, action, reward, terminated)
+
+                obs = new_obs
+                
+            # Last observation
+            buffer.append(torch.FloatTensor(obs), None, None, None)
+
+            print(f"update #{update + 1}: ", buffer.obs[0], buffer.obs.shape, buffer.actions[0], buffer.actions.shape, buffer.terminals)
+            buffer.reset()
+
+        self.envs.close()
 
 
 
 if __name__ == '__main__':
-    ppo = PPO('oracle', 'SpritesState-v0')
-    ppo.learn(total_timesteps=1000, log_interval=100)
+    ppo = PPO('oracle', 'SpritesState-v0', num_envs=4)
+    ppo.learn(total_timesteps=9192, log_interval=100)
