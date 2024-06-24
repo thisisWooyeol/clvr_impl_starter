3c897dfbc656c5e2cf20d4d23b6f6fdf38282326
thisiswooyeol <wooyeol0519@snu.ac.kr>
2024-06-24 19:48:06 +0900

    feat: implement working PPO on oracle, 0 distractor env

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -13,18 +13,24 @@ from reward_induced.src.reward_predictor import RewardPredictor, ImageEncoder
 
 class RolloutBuffer:
     def __init__(self, n_steps, n_envs, obs_shape, action_shape, device):
+        """
+        Length of each buffer:
+            obs, state_values, terminals: n_steps + 1
+            actions, action_logprobs, rewards: n_steps
+        """
         self.n_steps = n_steps
         self.current_step = 0
+        self.device = device
         self.obs = torch.zeros((self.n_steps + 1, n_envs, *obs_shape), dtype=torch.float32).to(device)
         self.actions = torch.zeros((self.n_steps, n_envs, *action_shape), dtype=torch.float32).to(device)
         self.action_logprobs = torch.zeros((self.n_steps, n_envs), dtype=torch.float32).to(device)
         self.state_values = torch.zeros((self.n_steps + 1, n_envs), dtype=torch.float32).to(device)
         self.rewards = np.zeros((self.n_steps, n_envs), dtype=np.float32)
-        self.terminals = np.zeros((self.n_steps, n_envs), dtype=np.float32)
+        self.terminals = np.zeros((self.n_steps + 1, n_envs), dtype=np.float32)
 
     def append(self, obs, action, action_logprob, state_value, reward, terminal):
         if self.current_step == self.n_steps:
-            self._append_last_obs(obs)
+            self._append_last_obs(obs, state_value, terminal)
         else:
             self.obs[self.current_step].copy_(obs)
             self.actions[self.current_step].copy_(action)
@@ -34,8 +40,10 @@ class RolloutBuffer:
             self.terminals[self.current_step] = terminal
             self.current_step += 1
 
-    def _append_last_obs(self, obs):
+    def _append_last_obs(self, obs, state_value, terminal):
         self.obs[-1].copy_(obs)
+        self.state_values[-1].copy_(state_value)
+        self.terminals[-1] = terminal
 
     def reset(self):
         self.current_step = 0
@@ -45,6 +53,24 @@ class RolloutBuffer:
         self.state_values = torch.zeros_like(self.state_values)
         self.rewards = np.zeros_like(self.rewards)
         self.terminals = np.zeros_like(self.terminals)
+
+    def compute_returns(self, gamma, gae_lambda):
+        # Use GAE to compute advantages
+        rewards = torch.from_numpy(self.rewards).to(self.device)
+        terminals = torch.from_numpy(self.terminals).to(self.device)    
+        next_state_values = self.state_values[1:]
+        curr_state_values = self.state_values[:-1]
+
+        # TD-errors per each step
+        deltas = rewards + gamma * next_state_values * (1 - terminals[1:]) - curr_state_values
+
+        gae = torch.zeros_like(terminals).to(self.device)
+        for t in reversed(range(self.n_steps)):
+            gae[t] = deltas[t] + gamma * gae_lambda * (1 - terminals[t+1]) * gae[t+1]
+        gae = gae[:-1]  # last gae value: used for for-loop computation, remove it
+        returns = gae + curr_state_values
+
+        return returns, gae
 
 
 class PPO(nn.Module):
@@ -77,6 +103,7 @@ class PPO(nn.Module):
         gamma: float,                           discount factor
         gae_lambda: float,                      factor for trade-off of bias vs variance for Generalized Advantage Estimator
         clip_range: float,                      clip range from (0, 1)
+        normalize_advantage: bool,              normalize advantage
         ent_coef: float,                        entropy coefficient
         vf_coef: float,                         value function coefficient
         tensorboard_log: str | None,            the log location for tensorboard (if None, no logging)
@@ -96,6 +123,7 @@ class PPO(nn.Module):
             gamma: float = 0.99,
             gae_lambda: float = 0.95,
             clip_range: float = 0.2,
+            normalize_advantage: bool = True,
             ent_coef: float = 0.01,
             vf_coef: float = 0.5,
             tensorboard_log: str = None,
@@ -123,6 +151,7 @@ class PPO(nn.Module):
         self.gamma = gamma
         self.gae_lambda = gae_lambda
         self.clip_range = clip_range
+        self.normalize_advantage = normalize_advantage
         self.ent_coef = ent_coef
         self.vf_coef = vf_coef
 
@@ -179,46 +208,65 @@ class PPO(nn.Module):
         
     def collect_rollouts(self, buffer: RolloutBuffer):
         obs, _ = self.envs.reset(seed=self.seed)
+        terminated = np.zeros(self.envs.num_envs)
         for _ in range(self.n_steps):
             obs = torch.FloatTensor(obs).to(self.device)
             with torch.no_grad():
                 action, action_logprob, state_value = self.policy.get_action(obs)
 
-            new_obs, reward, terminated, _, _ = self.envs.step(action.cpu().numpy())
+            next_obs, reward, next_terminated, _, _ = self.envs.step(action.cpu().numpy())
             buffer.append(obs, action, action_logprob, state_value, reward, terminated)
 
-            obs = new_obs
+            obs = next_obs
+            terminated = next_terminated
+            
         # Last observation
         obs = torch.FloatTensor(obs).to(self.device)
         with torch.no_grad():
             _, _, state_value = self.policy.get_action(obs)
-        buffer.append(obs, None, None, state_value, None, None)
+        buffer.append(obs, None, None, state_value, None, terminated)
+
+    def validate(self, num_episodes: int = 10):
+        """
+        Compute `num_episodes` * `num_envs` returns for the current policy
+        """
+        self.policy.to(self.device)
+        returns = []
+        for _ in range(num_episodes):
+            obs, _ = self.envs.reset()
+            total_reward = 0
+            done = np.asarray([False] * self.envs.num_envs)
+            while not done.all():  # is only working for SpritesEnv
+                obs = torch.FloatTensor(obs).to(self.device)
+                with torch.no_grad():
+                    action, _, _ = self.policy.get_action(obs)
+                obs, reward, done, _, _ = self.envs.step(action.cpu().numpy())
+                total_reward += reward
+            returns.append(total_reward)
+        self.envs.close()
+        returns = np.concatenate(returns)
+
+        return returns
+        
 
     def update(self, buffer: RolloutBuffer, logger: logging.Logger):
-        # FIXME: first use reward-to-go, then use GAE
-        rewards = torch.from_numpy(buffer.rewards).to(self.device)
-        is_terminals = torch.from_numpy(buffer.terminals).to(self.device)
-        advantages = torch.zeros_like(rewards).to(self.device)
-
-        # Compute rewards-to-go
-        for t in reversed(range(self.n_steps - 1)):
-            rewards[t] += self.gamma * rewards[t + 1] * (1 - is_terminals[t])
-        # Normalize rewards
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
-        # Compute advantages
-        advantages = rewards - buffer.state_values.squeeze(-1)
+        returns, advantages = buffer.compute_returns(self.gamma, self.gae_lambda)
+        if self.normalize_advantage:
+            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-7)
+            returns = advantages + buffer.state_values[:-1]
+        logger.debug(f'advantages: {advantages}')
+        logger.debug(f'returns: {returns}')
 
 
-        # FIXME: not a correct update implementation
         for _ in range(self.n_epochs):
+            shuffled_indices = torch.randperm(self.n_steps)
             for indices in range(0, self.n_steps, self.batch_size):
-                batch_indices = slice(indices, indices + self.batch_size)
+                batch_indices = shuffled_indices[indices:indices + self.batch_size]
                 obs_batch = buffer.obs[batch_indices]
                 action_batch = buffer.actions[batch_indices]
                 old_action_logprobs = buffer.action_logprobs[batch_indices]
-                old_state_values = buffer.state_values[batch_indices]  # TODO: is actually used for GAE 
-                returns = rewards[batch_indices]
-                advs = advantages[batch_indices]
+                advs_batch = advantages[batch_indices]
+                returns_batch = returns[batch_indices]
 
                 # Evaluate old actions
                 action_logprobs, state_values, dist_entropy = self.policy.evaluate(obs_batch, action_batch)
@@ -227,12 +275,12 @@ class PPO(nn.Module):
                 ratios = torch.exp(action_logprobs - old_action_logprobs)
 
                 # Compute surrogate loss
-                surr1 = ratios * advs
-                surr2 = torch.clamp(ratios, 1 - self.clip_range, 1 + self.clip_range) * advs
+                surr1 = ratios * advs_batch
+                surr2 = torch.clamp(ratios, 1 - self.clip_range, 1 + self.clip_range) * advs_batch
                 actor_loss = -torch.min(surr1, surr2).mean()
 
                 # Compute value loss
-                critic_loss = (state_values - returns).pow(2).mean()
+                critic_loss = (state_values - returns_batch).pow(2).mean()
 
                 # Compute total loss
                 loss = actor_loss + self.ent_coef * critic_loss - self.ent_coef * dist_entropy.mean()
@@ -242,10 +290,10 @@ class PPO(nn.Module):
                 loss.backward()
                 self.optimizer.step()
 
-            # Log
-            logger.debug(f'    actor loss:      {actor_loss.item()}')
-            logger.debug(f'    critic loss:     {critic_loss.item()}')
-            logger.debug(f'    entropy:         {dist_entropy.mean().item()}')
+        # Log
+        logger.debug(f'    actor loss:      {actor_loss.item()}')
+        logger.debug(f'    critic loss:     {critic_loss.item()}')
+        logger.debug(f'    entropy:         {dist_entropy.mean().item()}')
 
     def learn(
             self, 
@@ -273,7 +321,11 @@ class PPO(nn.Module):
                 - N epochs:         {self.n_epochs}
                 - Batch size:       {self.batch_size}
                 - gamma (discount): {self.gamma}
+                - GAE lambda:       {self.gae_lambda}
                 - clip range:       {self.clip_range}
+                - Normalize adv:    {self.normalize_advantage}
+                - Entropy coef:     {self.ent_coef}
+                - Value coef:       {self.vf_coef}
                 - Optimizer:        Adam
                 - Learning rate:    {self.learning_rate}
             Device
@@ -284,15 +336,20 @@ class PPO(nn.Module):
         """)
 
 
-
         for update in range(total_timesteps // (self.n_steps * self.envs.num_envs)):
-            logger.info(f'[INFO] Update {update}')
+            logger.info(f'[INFO] Update {update} ({update * self.n_steps * self.envs.num_envs} / {total_timesteps} steps)')
             self.collect_rollouts(buffer)
             self.update(buffer, logger)
             buffer.reset()
 
+            if self.verbose > 0:
+                returns = self.validate()
+                logger.info(f'    (Validation) Returns: {np.mean(returns)} +/- {np.std(returns)}')
+        logger.info(f'[INFO] Training finished ({total_timesteps} steps)')
+
         self.envs.close()
-        self.save(f'ppo/models/{self.policy_type}-{total_timesteps}steps.pt')
+        self.save(f'ppo/models/{self.policy_type}/{self.policy_type}-{total_timesteps}steps.pt')
+        logger.info(f'[INFO] Model saved at ppo/models/{self.policy_type}-{total_timesteps}steps.pt')
 
     def save(self, path):
         torch.save(self.policy.state_dict(), path)
@@ -316,5 +373,5 @@ def _setup_logger(log_file_path, verbose):
 
 
 if __name__ == '__main__':
-    ppo = PPO('oracle', 'SpritesState-v0', num_envs=4, verbose=2)
-    ppo.learn(total_timesteps=2**20, log_interval=100)
+    ppo = PPO('oracle', 'SpritesState-v0', num_envs=4, verbose=1)
+    ppo.learn(total_timesteps=2**20, log_interval=10)
